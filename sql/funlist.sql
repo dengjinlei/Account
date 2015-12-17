72d32863f2304317d6bbc52d60a179f0c8f788a3
dengjinlei <dengjinlei@aliyun.com>
2015-12-17 22:38:58 +0800

    首页功能列表配置数据

---
+++ sql/funlist.sql
@@ -0,0 +1,7 @@
+INSERT INTO `funlist` (`id`, `context`, `level`, `name`, `position`, `status`, `url`) VALUES (1, '', 3, '账目类型', 1, 0, 'costtype_list.action');
+INSERT INTO `funlist` (`id`, `context`, `level`, `name`, `position`, `status`, `url`) VALUES (2, '', 2, '用户管理 ', 2, 0, 'spender_list.action');
+INSERT INTO `funlist` (`id`, `context`, `level`, `name`, `position`, `status`, `url`) VALUES (3, '', 3, '账单管理', 3, 0, 'costinfo_list.action');
+INSERT INTO `funlist` (`id`, `context`, `level`, `name`, `position`, `status`, `url`) VALUES (4, '', 1, '功能列表管理 ', 4, 0, 'funlist_list.action');
+INSERT INTO `funlist` (`id`, `context`, `level`, `name`, `position`, `status`, `url`) VALUES (5, 'jdbc 测试', 3, '试试 ', 6.01, 0, '/dbs/pages/jQuery/shhi.jsp');
+INSERT INTO `funlist` (`id`, `context`, `level`, `name`, `position`, `status`, `url`) VALUES (6, '', 3, 'ajax_json', 5, 0, 'ajaxjson.action');
+INSERT INTO `funlist` (`id`, `context`, `level`, `name`, `position`, `status`, `url`) VALUES (7, '登录状态校验测试', 1, '登录状态 ', 66, 1, 'shhi.action');
