89e1b2c956978ba51239c57e855e402888dfddd5
dengjinlei <dengjinlei@aliyun.com>
2015-12-17 22:23:34 +0800

    注释无用代码

--- src/com/djl/util/LoginFilter.java
+++ src/com/djl/util/LoginFilter.java
@@ -18,40 +18,39 @@ public class LoginFilter implements Filter {
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-			System.out.println("开始校验");
+//			System.out.println("开始校验");
 			HttpServletRequest servletRequest = (HttpServletRequest) request;
 			HttpServletResponse servletResponse = (HttpServletResponse) response;
 			HttpSession session = servletRequest.getSession();
 			
-			//
 			String path = servletRequest.getRequestURI();
 			System.out.println(path);
-			Spender user = (Spender) session.getAttribute("userContext"); 
 			
 			if(path.indexOf("jsp")>-1||path.indexOf("action")>-1){
 				if( path.indexOf("login_userLogin")>-1){ 
-					System.out.println("登录页面跳过校验");
+//					System.out.println("登录页面跳过校验");
 					chain.doFilter(servletRequest, servletResponse);
 				}
 				else{
+					Spender user = (Spender) session.getAttribute("userContext"); 
 					if(user==null){
-						System.out.println("校验不通过，跳转到登录页面!!!");
+						//未登录，进行登录校验
+//						System.out.println("校验不通过，跳转到登录页面!!!");
 						servletResponse.sendRedirect("login_userLogin.action");
 					}else{
-						System.out.println("校验通过，继续操作 , 登录用户："+user.getName());
+//						System.out.println("校验通过，继续操作 , 登录用户："+user.getName());
 						chain.doFilter(servletRequest,servletResponse);
 					}
 				}
 			}
 			else{
-				System.out.println("不在校验范围，跳过校验");
+//				System.out.println("不在校验范围，跳过校验");
 				chain.doFilter(servletRequest, servletResponse);
 			}
 			
